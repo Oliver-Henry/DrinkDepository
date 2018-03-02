995c290f63a0bc5182c7241324733808733e464c
Oliver-Henry <36078057+Oliver-Henry@users.noreply.github.com>
2018-03-02 21:35:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DrinkDepository
---
+++ app/src/main/java/com/example/olive/drinkdepository/data/network/service/ApiList.java
@@ -0,0 +1,15 @@
+package com.example.olive.drinkdepository.data.network.service;
+
+/**
+ * Created by olive on 02/03/2018.
+ */
+
+public class ApiList {
+    public static final String BASE_URL = "https://www.thecocktaildb.com/api/json/v1/";
+    public static final String API_KEY = "1";
+
+
+
+
+
+}
